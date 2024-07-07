3080bd67dc8cc10efbf691f4f0b3c981d0c2bc1b
Matheus <matheus.lazaro2002@gmail.com>
2024-07-07 18:35:23 -0300

    função de deletar livro

--- src/components/private/admin/DelUser.jsx
+++ src/components/private/admin/DelUser.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { useState } from "react";
 
 export default function DelUser() {
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const handleSubmit = async (e) => {};
+
+    const handleChange = (e) => {};
+
     return (
         <>
-            <p>
-                DEL USER Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum qui
-                nemo, odio asperiores quia eaque? Voluptate modi consequuntur asperiores nobis
-                ducimus quis quas, molestiae, magni enim amet, consequatur facilis!
-            </p>
+            <form className="form-adm" onSubmit={handleSubmit}>
+                <h3>Deletar usuário</h3>
+                <div className="input-item">
+                    <label htmlFor="id">Id do usuário</label>
+                    <input id="titulo" name="titulo" type="text" onChange={handleChange} />
+                </div>
+                <p className="error">{errorMsg}</p>
+                <button type="submit">Criar Livro</button>
+            </form>
         </>
     );
 }
