1f586b21f2b00444f0ed37f43240560974442fd3
Gabriel <gbfsap.gf@gmail.com>
2024-07-07 21:40:41 -0300

    adiciona função deleta usuário

--- src/components/private/admin/DelUser.jsx
+++ src/components/private/admin/DelUser.jsx
@@ -1,23 +1,47 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
+import { getAuth } from "../../../services/auth";
+import { BACK_URL } from "../../../services/serverConection";
 
 export default function DelUser() {
     const [errorMsg, setErrorMsg] = useState("");
+    const [idUser, setIdUser] = useState("")
 
-    const handleSubmit = async (e) => {};
+    const config = {
+        headers: {
+            Authorization: "Bearer " + getAuth()
+        }
+      }
 
-    const handleChange = (e) => {};
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const url = `${BACK_URL}/usuarios/${idUser}`
+        try{
+            const res = await axios.delete(url, config)
+            setErrorMsg(res.data.msg)
+        }
+        catch(error){
+            setErrorMsg(error.response.data.msg)
+        }
+    };
+
+    const handleChange = (e) => {
+        setErrorMsg("")
+        setIdUser(e.target.value)
+    };
 
     return (
         <>
             <form className="form-adm" onSubmit={handleSubmit}>
                 <h3>Deletar usuário</h3>
                 <div className="input-item">
-                    <label htmlFor="id">Id do usuário</label>
-                    <input id="titulo" name="titulo" type="text" onChange={handleChange} />
+                    <label htmlFor="idUser">Id do usuário</label>
+                    <input id="idUser" name="idUser" type="text" onChange={handleChange} />
                 </div>
                 <p className="error">{errorMsg}</p>
-                <button type="submit">Criar Livro</button>
+                <button type="submit">Deletar</button>
             </form>
         </>
     );
